9d3d2f0d93a2a540089971943174e25634d0dcfc
HoffiMuc <hoffi.mail@gmail.com>
2020-01-11 10:12:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-echo "$(date) ${HOME}/env/setup.sh ran" >> ${HOME}/install.log
+echo "$(date) ${HOME}/env/setup.sh start..." >> ${HOME}/install.log
+
+sudo apt update
+sudo apt --yes upgrade
+
+sudo apt --yes install httpie 
+
+#wget -qO - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/public.key | sudo apt-key add -
+#echo "deb http://apt.starkandwayne.com stable main" | sudo tee /etc/apt/sources.list.d/starkandwayne.list
+#sudo apt update
+sudo apt --yes install direnv spruce
+
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+#sudo touch /etc/apt/sources.list.d/kubernetes.list 
+#echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+#sudo apt update
+sudo apt --yes install kubectl
+
+echo "$(date) ${HOME}/env/setup.sh end." >> ${HOME}/install.log
 
